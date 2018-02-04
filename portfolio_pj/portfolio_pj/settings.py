c5a467723e800ba7bb8e3620ffa434da515f47e1
nguyentrisinh <nguyentrisinh0810@gmail.com>
2018-02-04 11:40:52 +0700

    add static root and allow host

--- portfolio_pj/portfolio_pj/settings.py
+++ portfolio_pj/portfolio_pj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'lhk!7v1lq-00#qkr^6l@*r*&%px6(nrgusn)pz!v40uovwmm=2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.104.173.244']
 
 
 # Application definition
@@ -121,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
