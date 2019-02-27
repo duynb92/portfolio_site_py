6efc465b78a6d6214159753dc01bc735617c0d35
Duy Nguyen <duynb92@gmail.com>
2019-02-28 00:50:53 +0700

    set STATIC PATH to default

--- portfolio_pj/portfolio_pj/settings.py
+++ portfolio_pj/portfolio_pj/settings.py
@@ -131,10 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-# STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
-STATIC_URL = 'app/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'app/static/')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+# STATIC_URL = 'app/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'app/static/')
 
 # CKEDITOR_BASEPATH = '/static/'
 CKEDITOR_CONFIGS = {
