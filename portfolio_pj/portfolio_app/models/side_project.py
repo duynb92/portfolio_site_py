3b9fab1bf26163f0843fa608b06fb2ec9ed251ec
Duy <duy@duynb.com>
2026-04-24 22:55:33 +0800

    Add apple,google links

--- portfolio_pj/portfolio_app/models/side_project.py
+++ portfolio_pj/portfolio_app/models/side_project.py
@@ -1,7 +1,9 @@
 class SideProject:
-    def __init__(self, name, thumbnail, tech_tags, github_url="", demo_url=""):
+    def __init__(self, name, thumbnail, tech_tags, github_url="", demo_url="", apple_store_url="", google_play_url=""):
         self.name = name
         self.thumbnail = thumbnail
         self.tech_tags = tech_tags
         self.github_url = github_url
         self.demo_url = demo_url
+        self.apple_store_url = apple_store_url
+        self.google_play_url = google_play_url
